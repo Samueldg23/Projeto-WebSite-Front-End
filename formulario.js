29bc501fd3a3baacf2a278f0672171d6d1bd8eff
Samuel_Dg <samueldgomes00@gmail.com>
2025-04-24 23:40:58 -0300

    ajustes final

--- formulario.js
+++ formulario.js
@@ -8,7 +8,7 @@ window.onload = function () {
 
   cursos.forEach((curso, index) => {
     const option = document.createElement("option");
-    option.value = index; 
+    option.value = index;
     option.textContent = `${curso.curso} (${curso.ano})`;
     selectCurso.appendChild(option);
   });
@@ -25,13 +25,14 @@ window.onload = function () {
 
   if (editarCursoIndex !== null && editarPeriodoIndex !== null) {
     const cursoSelecionado = cursos[editarCursoIndex];
-    const periodoSelecionado = cursoSelecionado.periodos[editarPeriodoIndex];
+    const periodoSelecionado = cursoSelecionado?.periodos?.[editarPeriodoIndex];
 
-    selectCurso.value = editarCursoIndex;
-    selectTurma.value = periodoSelecionado.turma;
-    inputNomePeriodo.value = periodoSelecionado.nome;
-
-    document.querySelector('h1').textContent = "Editar Período";
+    if (cursoSelecionado && periodoSelecionado) {
+      selectCurso.value = editarCursoIndex;
+      selectTurma.value = periodoSelecionado.turma;
+      inputNomePeriodo.value = periodoSelecionado.nome;
+      document.querySelector('h1').textContent = "Editar Período";
+    }
 
     localStorage.removeItem('editarCursoIndex');
     localStorage.removeItem('editarPeriodoIndex');
@@ -51,14 +52,13 @@ window.onload = function () {
     }
 
     const cursoSelecionado = cursos[cursoIndex];
-
     const novoPeriodo = {
       nome: nomePeriodo,
       turma: turmaSelecionada
     };
 
     if (editarCursoIndex !== null && editarPeriodoIndex !== null) {
-      cursoSelecionado.periodos[editarPeriodoIndex] = novoPeriodo;
+      cursos[editarCursoIndex].periodos[editarPeriodoIndex] = novoPeriodo;
     } else {
       if (!cursoSelecionado.periodos) {
         cursoSelecionado.periodos = [];
@@ -67,7 +67,6 @@ window.onload = function () {
     }
 
     localStorage.setItem("controlePeriodos", JSON.stringify(cursos));
-
     alert("Período salvo com sucesso!");
     window.location.href = "index.html";
   });
